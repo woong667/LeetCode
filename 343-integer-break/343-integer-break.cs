3366cfb221ab07bf8b2cc491e409ed26c1b26396
KUN WOONG <66250847+woong667@users.noreply.github.com>
2022-03-29 15:44:26 +0900

    Time: 57 ms (5.36%), Space: 26 MB (16.07%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cs
@@ -0,0 +1,55 @@
+public class Solution {
+    public int IntegerBreak(int n) {
+         int left;
+            int right;
+            int result;
+            int maxResult;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            
+            dic[2] = 1;
+            dic[3] = 2;
+            dic[4] = 4;
+
+            for(int i=5;i<=58;i++)
+            {
+                result = 1;
+                maxResult = -1;
+                left = 2;
+                right = i-2;
+                while(left<=right)
+                {
+                    if(left>dic[left])
+                    {
+                        result *= left;
+                    }
+                    else
+                    {
+                        result *= dic[left];
+                    }
+
+                    if(right>dic[right])
+                    {
+                        result *= right;
+                    }
+                    else
+                    {
+                        result *= dic[right];
+                    }
+
+                    if(maxResult<result)
+                    {
+                        maxResult = result;
+                    }
+
+                    result = 1;
+                    left++;
+                    right--;
+                }
+                dic[i] = maxResult;
+
+
+            }
+            
+            return dic[n];
+    }
+}
